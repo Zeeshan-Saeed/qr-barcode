fbba7a121856b78e719ed12e04f0057efd09f482
Michael Troger <michaeltroger@users.noreply.github.com>
2016-12-01 13:40:46 +0100

    package renaming + little code refactoring

--- app/src/androidTest/java/com/michaeltroger/qrbarcode/ApplicationTest.java
+++ app/src/androidTest/java/com/michaeltroger/qrbarcode/ApplicationTest.java
@@ -1,4 +1,4 @@
-package be.pxl.troger.ar;
+package com.michaeltroger.qrbarcode;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/com/michaeltroger/qrbarcode/ExampleUnitTest.java
+++ app/src/test/java/com/michaeltroger/qrbarcode/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package be.pxl.troger.ar;
+package com.michaeltroger.qrbarcode;
 
 import org.junit.Test;
 
